2762cddbabe1ec55ecfcec8bc6c62e184a0a68ed
Patrick Harrison <jahman@bellatlantic.net>
2026-08-12 09:45:29 -0400

    Add /business rewrite to Harricom

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -9,8 +9,19 @@ const nextConfig = {
   transpilePackages: [
     "@prodigal/ui",
     "@prodigal/utils"
-  ]
+  ],
+  async rewrites() {
+    return [
+      {
+        source: '/business',
+        destination: 'https://harricom.vercel.app/business',
+      },
+      {
+        source: '/business/:path*',
+        destination: 'https://harricom.vercel.app/business/:path*',
+      },
+    ]
+  }
 };
 
 module.exports = nextConfig;
-
